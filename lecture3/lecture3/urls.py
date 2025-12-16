0dc5ceb5d6768851350d33fc08c0462cc1380949
Neel <neelgundale@gmail.com>
2025-12-16 12:58:01 +0530

    Updated urlpatterns

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import include, path
 
 urlpatterns = [
+    path("newyear/", include("newyear.urls")),
+    path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls),
-    path('hello/', include("hello.urls"))
 ]
