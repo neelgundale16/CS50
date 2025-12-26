0621b2713f938818c28714345a002982299054ad
Neel <neelgundale@gmail.com>
2025-12-26 15:43:43 +0530

    Added column mode

--- queries.sql
+++ queries.sql
@@ -10,5 +10,9 @@ INSERT INTO flights (origin, destination, duration) VALUES
     ('Tokyo', 'Sydney', 600),
     ('Mumbai', 'Dubai', 190),
     ('Berlin', 'Rome', 120);
+    ('New York', 'London', 400);
+    ('San Francisco', 'Tokyo', 540);
+    ('Beijing', 'Moscow', 480);
+
 
 SELECT * FROM flights;    
