d2ebbe93d675b1d66234547a22088170bc725396
Neel <neelgundale@gmail.com>
2025-12-26 18:05:33 +0530

    Update queries

--- queries.sql
+++ queries.sql
@@ -9,10 +9,13 @@ INSERT INTO flights (origin, destination, duration) VALUES
     ('Delhi', 'Paris', 510),
     ('Tokyo', 'Sydney', 600),
     ('Mumbai', 'Dubai', 190),
-    ('Berlin', 'Rome', 120);
-    ('New York', 'London', 400);
-    ('San Francisco', 'Tokyo', 540);
+    ('Berlin', 'Rome', 120),
+    ('New York', 'London', 400),
+    ('San Francisco', 'Tokyo', 540),
     ('Beijing', 'Moscow', 480);
 
 
 SELECT * FROM flights;    
+
+.mode columns
+.headers yes
