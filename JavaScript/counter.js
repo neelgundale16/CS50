a183a7b22996b7b6cf620fa0539f429fa16f4223
Neel <neelgundale@gmail.com>
2026-01-05 23:52:42 +0530

    Updated count function

--- JavaScript/counter.js
+++ JavaScript/counter.js
@@ -1,15 +1,19 @@
-   let counter = 0;
+   if(localStorage.getItem("counter")){
+        localStorage.setItem("counter" , 0);
+   }
         function count() {
+            let counter = localStorage.getItem("counter");
             counter++;
-            alert(counter);
             document.querySelector("h1").innerHTML = counter;
+            localStorage.setItem("counter", counter);
             
-            if (counter % 5 === 0) {
-                alert("Count is now " + counter);
-            } 
         }
     document.addEventListener("DOMContentLoaded", function() {
 
-        document.querySelector('button').addEventListener('click', count);
+    document.querySelector('h1').innerHTML = localStorage.getItem("counter");
+    document.querySelector('button').addEventListener('click', count);
+
+        setInterval(count, 1000);
+
    
     });
